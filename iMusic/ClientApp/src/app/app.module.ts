922ddb799b1dc57dcf332599350b024e3e5dcfed
Petros Sargsian <petros.sargsian@gmail.com>
2020-12-26 21:58:58 +0200

    Implemented logout

--- iMusic/ClientApp/src/app/app.module.ts
+++ iMusic/ClientApp/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopnavbarComponent } from './topnavbar/topnavbar.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtTokenInterceptorService } from './core/interceptors/jwt-token-interceptor';
+import { AuthService } from './core/custom/api/auth-service';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,13 @@ import { JwtTokenInterceptorService } from './core/interceptors/jwt-token-interc
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtTokenInterceptorService, multi: true }], 
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtTokenInterceptorService, multi: true },
+    AuthService
+  ], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
