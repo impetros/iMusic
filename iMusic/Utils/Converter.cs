3e0aae8e048f0562660fbcf3cf39cbb4ce3372ea
Petros Sargsian <petros.sargsian@gmail.com>
2020-12-28 15:55:52 +0200

    Created Converter for string -> byte array and byte array -> string

---
+++ iMusic/Utils/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace iMusic.Utils
+{
+    public class Converter
+    {
+        public static string GetStringFromByteArray(Byte[] buffer)
+        {
+            if (buffer == null) return String.Empty;
+            return System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        }
+
+        public static Byte[] GetByteArrayFromString(string convert)
+        {
+            if (String.IsNullOrEmpty(convert)) return null;
+            return System.Text.Encoding.UTF8.GetBytes(convert);
+        }
+    }
+}
