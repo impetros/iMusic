07c3a235414a6fe097d89a86127f5f7c9242fbc6
Petros Sargsian <petros.sargsian@gmail.com>
2020-12-29 00:10:31 +0200

    Started implementing Shopping Cart functionality

--- iMusic/ClientApp/src/app/core/swagger/model/models.ts
+++ iMusic/ClientApp/src/app/core/swagger/model/models.ts
@@ -2,4 +2,6 @@ export * from './albumDTO';
 export * from './artistDTO';
 export * from './loginRequest';
 export * from './loginResponse';
+export * from './shoppingCartDTO';
+export * from './shoppingCartItemDTO';
 export * from './songDTO';
