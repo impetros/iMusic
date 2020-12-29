1f383cb39d4d82a21830ec5a006f05c18479f6c8
Petros Sargsian <petros.sargsian@gmail.com>
2020-12-29 11:36:23 +0200

    Now we can add songs in the shopping cart

--- iMusic/ClientApp/src/app/core/custom/cart.service.ts
+++ iMusic/ClientApp/src/app/core/custom/cart.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { ShoppingCartDTO, ShoppingCartItemDTO } from '../swagger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  private readonly CART: string = "iMusic-Cart";
   constructor() { }
+
+  public getCart(): ShoppingCartDTO {
+    let stringCART = sessionStorage.getItem(this.CART);
+    if(stringCART == null){
+      this.initEmptyCart();
+      return this.getCart();
+    };
+    return JSON.parse(stringCART) as ShoppingCartDTO;
+  }
+
+  public getNumberOfItems() : number {
+    let shoppingCart = this.getCart();
+    if(shoppingCart == null || shoppingCart.cartitems == null) return 0;
+    return shoppingCart.cartitems.length;
+  }
+
+  public addItem(songId: number | undefined, albumId: number | undefined, price: number | undefined) { 
+    if(songId == albumId || (songId != null && albumId != null)) return; // only one of them should be non-nullable
+    let cart = this.getCart();
+    let shoppingCartItem: ShoppingCartItemDTO = {
+      albumId: albumId,
+      songId: songId,
+      price: price
+    };
+    cart.cartitems?.push(shoppingCartItem);
+    this.setCart(cart);
+  }
+
+  private initEmptyCart(){
+    let shoppingCart: ShoppingCartDTO = {
+      cartitems: []
+    };
+    this.setCart(shoppingCart);
+  }
+
+  private setCart(cart: ShoppingCartDTO) {
+    sessionStorage.setItem(this.CART, JSON.stringify(cart));
+  }
+
 }
--- iMusic/ClientApp/src/app/shared/song-card/song-card.component.ts
+++ iMusic/ClientApp/src/app/shared/song-card/song-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CartService } from 'src/app/core/custom/cart.service';
 import { SongDTO } from 'src/app/core/swagger';
 
 @Component({
@@ -10,9 +11,24 @@ export class SongCardComponent implements OnInit {
   @Input()
   song!: SongDTO;
   
-  constructor() { }
+  artists: string = "";
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    if(this.song && this.song.artists){
+      for(let i = 0; i < this.song.artists?.length; i ++) {
+        if(i === 0 || i === this.song.artists?.length - 1) {
+          this.artists += this.song.artists[i].name;
+        } else {
+          this.artists += this.song.artists[i].name + ", ";
+        }
+      }
+    }
+  }
+
+  addItemToCart() {
+    this.cartService.addItem(this.song?.songId, undefined, this.song.price);
   }
 
 }
