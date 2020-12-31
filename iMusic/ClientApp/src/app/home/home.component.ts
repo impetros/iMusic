89647c72f67cad95bad8538d4aaa11f7c14e55ec
Petros Sargsian <petros.sargsian@gmail.com>
2020-12-31 11:47:50 +0200

    Created home page

--- iMusic/ClientApp/src/app/home/home.component.ts
+++ iMusic/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../core/custom/cart.service';
+import { AlbumDTO, AlbumService, ArtistDTO, ArtistService, SongDTO, SongService } from '../core/swagger';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public songs!: SongDTO[];
+  public artists!: ArtistDTO[];
+  public albums!: AlbumDTO[];
+
+  constructor(private cartService: CartService, private songService: SongService, private artistService: ArtistService,
+    private albumService: AlbumService) { }
 
   ngOnInit(): void {
+    this.songService.getAllSongs().subscribe((songs: SongDTO[]) => {
+      this.songs = songs;
+    })
+    this.artistService.getAllArtists().subscribe((artists: ArtistDTO[]) => {
+      this.artists = artists;
+    });
+    this.albumService.getAllAlbums().subscribe((albums: AlbumDTO[]) => {
+      this.albums = albums;
+    });
+  }
+
+  addAlbumInCartEvent($event: AlbumDTO) {
+    this.cartService.addItem(null, $event, $event.price);
+  }
+
+  addSongInCartEvent($event: SongDTO) {
+    this.cartService.addItem($event, null, $event.price);
   }
 
 }
--- iMusic/ClientApp/src/app/home/home.module.ts
+++ iMusic/ClientApp/src/app/home/home.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
+import { AlbumService, ArtistService, SongService } from '../core/swagger';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    SharedModule
+  ],
+  providers:[AlbumService, ArtistService, SongService]
 })
 export class HomeModule { }
